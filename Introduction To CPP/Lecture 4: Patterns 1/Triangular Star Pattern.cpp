2d4922bad1fd34b3505834f9cf5fe895f3d7dad1
AshuHack0 <109748201+AshuHack0@users.noreply.github.com>
2022-10-30 22:11:45 +0530

    Create Triangular Star Pattern.cpp

---
+++ Introduction To CPP/Lecture 4: Patterns 1/Triangular Star Pattern.cpp
@@ -0,0 +1,48 @@
+/*
+Print the following pattern for the given N number of rows.
+Pattern for N = 4
+*
+**
+***
+****
+Note : There are no spaces between the stars (*).
+Input format :
+Integer N (Total no. of rows)
+Output format :
+Pattern in N lines
+Constraints
+0 <= N <= 50
+Sample Input 1:
+5
+Sample Output 1:
+*
+**
+***
+****
+*****
+Sample Input 2:
+6
+Sample Output 2:
+*
+**
+***
+****
+*****
+******
+*/
+#include<iostream>
+using namespace std;
+int main(){
+     int n; 
+    cin >> n; 
+    for (int i=0 ; n>i;i++)
+    {
+        for (int j=0; i>=j;j++)
+        {
+            cout << "*" ;
+        }
+        cout << endl; 
+    }        
+}
+
+
