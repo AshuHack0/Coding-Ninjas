c144616185fa60404dc0c51c858169f50e60c06f
AshuHack0 <109748201+AshuHack0@users.noreply.github.com>
2022-10-30 18:31:33 +0530

    Create  Square Pattern.cpp

---
+++ Introduction To CPP/Lecture 4: Patterns 1/Square Pattern.cpp
@@ -0,0 +1,25 @@
+/*
+Print the following pattern for the given N number of rows.
+Pattern for N = 4
+4444
+4444
+4444
+4444
+*/
+
+  #include<iostream>
+using namespace std;
+
+
+int main(){
+    int N;
+    cin >>N;
+    int i=1;
+
+    while (i <=N){int j = 1;
+    while (j <=N){cout <<N;
+j=j+1;
+    } cout <<endl;
+    i=i+1;} 
+}
+
