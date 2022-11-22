84102d40b2b17410760620d21f69af2bb82e1e11
Ashutosh Kumar <109748201+AshuHack0@users.noreply.github.com>
2022-11-22 11:21:29 +0530

    Create Minimum Bracket reversal .cpp

---
+++ Data Structures And Algorithm/Stack And queue/Minimum Bracket reversal .cpp
@@ -0,0 +1,93 @@
+/*
+Minimum bracket Reversal
+Send Feedback
+For a given expression in the form of a string, find the minimum number of brackets that can be reversed in order to make the expression balanced. The expression will only contain curly brackets.
+If the expression can't be balanced, return -1.
+Example:
+Expression: {{{{
+If we reverse the second and the fourth opening brackets, the whole expression will get balanced. Since we have to reverse two brackets to make the expression balanced, the expected output will be 2.
+
+Expression: {{{
+In this example, even if we reverse the last opening bracket, we would be left with the first opening bracket and hence will not be able to make the expression balanced and the output will be -1.
+Input Format :
+The first and the only line of input contains a string expression, without any spaces in between.
+Output Format :
+The only line of output will print the number of reversals required to balance the whole expression. Prints -1, otherwise.
+Note:
+You don't have to print anything. It has already been taken care of.
+Constraints:
+0 <= N <= 10^6
+Where N is the length of the expression.
+
+Time Limit: 1sec
+Sample Input 1:
+{{{
+Sample Output 1:
+-1
+Sample Input 2:
+{{{{}}
+Sample Output 2:
+1
+
+*/
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+  #include <stack>
+int countBracketReversals(string input){
+	// Write your code here
+     int len=input.length();
+     
+     if(len==0)
+     {
+         return 0;
+     }
+     if(len%2!=0)   // if odd then no matter it always unbalanced;
+     {
+         return -1;
+     }
+     stack<char> s;
+      for (int i=0 ;len>i;i++)
+      {
+          if( input[i]=='{')
+            {
+                s.push(input[i]);
+            }
+            else
+            {
+                if (!s.empty() && s.top()=='{')
+                {
+                    s.pop();
+                }
+                else {
+                    s.push(input[i]);
+                }
+            }
+      }
+      int count=0;
+      while(!s.empty())
+      {
+          char f1=s.top();
+        s.pop();
+        char f2=s.top();
+        s.pop();
+            if(f1!=f2){
+                count=+2;
+               }
+               else
+               {
+                   count++;
+               }
+      }
+      return count;
+
+}
+
+int main() {
+    string input;
+    cin >> input;
+    cout << countBracketReversals(input);
+}
